a0f7ebce12c781c8846353794f74214246d7d06b
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2021-09-29 17:10:05 +0100

    creation of response objects

---
+++ DeveloperHubAPI/V1/Boundary/Response/DeveloperHubResponse.cs
@@ -0,0 +1,14 @@
+namespace DeveloperHubAPI.V1.Boundary.Response
+{
+    public class DevelopmentHubResponse
+    {
+        public string DevelopmentLinks { get; set;}
+        /// <example>https://github.com/</example>
+
+        public string GithubLink { get; set; }
+        ///<example>https://github.com/</example>
+
+         public string StagingLinks { get; set; }
+        /// <example>https://github.com/</example>
+    }
+}
