e61136eca3ea650c54b3f220cc984e0f976ca1d1
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2021-09-29 18:20:17 +0100

    renaming use case files

--- DeveloperHubAPI/V1/UseCase/Interfaces/IGetByIdUseCase.cs
+++
@@ -1,9 +0,0 @@
-using DeveloperHubAPI.V1.Boundary.Response;
-
-namespace DeveloperHubAPI.V1.UseCase.Interfaces
-{
-    public interface IGetByIdUseCase
-    {
-        ResponseObject Execute(int id);
-    }
-}
