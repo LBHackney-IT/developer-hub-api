aa03164685437602b4024c7095ddf4445b49ad5f
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2021-09-29 17:17:02 +0100

    updated the response obj with the necessary fields

--- DeveloperHubAPI/V1/Boundary/Response/DeveloperHubResponse.cs
+++ DeveloperHubAPI/V1/Boundary/Response/DeveloperHubResponse.cs
@@ -2,13 +2,31 @@ namespace DeveloperHubAPI.V1.Boundary.Response
 {
     public class DevelopmentHubResponse
     {
-        public string DevelopmentLinks { get; set;}
-        /// <example>https://github.com/</example>
+        public int Id { get; set; }
+        ///<example>123</example>
+        
+        public string  ApiName { get; set; }
+        ///<example>Developer Hub</example>
+
+        public string  Description { get; set; }
+        ///<example>Lorem ipsum dolor sit amet, consectetur adipiscing elit</example>
 
         public string GithubLink { get; set; }
         ///<example>https://github.com/</example>
 
+        public string  SwaggerLink { get; set; }
+        ///<example>https://github.com/</example>
+
+        public string DevelopmentLinks { get; set;}
+        /// <example>https://github.com/</example>
+
          public string StagingLinks { get; set; }
         /// <example>https://github.com/</example>
+
+        public string  StagingBaseURL { get; set; }
+        ///<example>https://github.com/</example>
+
+        public string  ApiSpecificationLink { get; set; }
+        ///<example>https://github.com/</example>
     }
 }
