7f0a4a267e37255aca1ad5a582c12c6fce2aba7b
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2021-09-29 10:11:21 +0100

    creation of response objects

---
+++ DeveloperHubAPI/V1/Boundary/Response/GithubLinks.cs
@@ -0,0 +1,8 @@
+namespace DeveloperHubAPI.V1.Boundary.Response
+{
+        public class GithubLinks
+    {
+        public string GithubLink { get; set; }
+        ///<example>https://github.com/</example>
+    }
+}
