1e9b84b44e6e416c7aa6a7485032d5d5ba1ad05e
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2021-09-29 17:11:12 +0100

    renaming and tidying

--- DeveloperHubAPI/V1/Boundary/Response/DevcelopmentLinks.cs
+++
@@ -1,9 +0,0 @@
-namespace DeveloperHubAPI.V1.Boundary.Response
-{
-    //TODO: Rename to represent to object you will be returning eg. ResidentInformation, HouseholdDetails e.t.c
-    public class DevelopmentLinks
-    {
-        public string DevelopmentLinks { get; set;}
-        /// <example>https://github.com/</example>
-    }
-}
--- DeveloperHubAPI/V1/Boundary/Response/GithubLinks.cs
+++
@@ -1,8 +0,0 @@
-namespace DeveloperHubAPI.V1.Boundary.Response
-{
-        public class GithubLinks
-    {
-        public string GithubLink { get; set; }
-        ///<example>https://github.com/</example>
-    }
-}
