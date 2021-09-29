11a8d832424d3cae05a73cf3d7e077488ac01c3e
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2021-09-29 17:10:22 +0100

    creation of domain object

---
+++ DeveloperHubAPI/V1/Domain/DeveloperHub.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace DeveloperHubAPI.V1.Domain
+{
+      public class DeveloperHub
+    {
+        public int Id { get; set; }
+        
+        public string  ApiName { get; set; }
+
+        public string  Description { get; set; }
+
+        public string  GithubLink { get; set; }
+
+        public string  SwaggerLink { get; set; }
+
+        public string  DevelopmentBaseURL { get; set; }
+
+        public string  StagingBaseURL { get; set; }
+
+        public string  ApiSpecificationLink { get; set; }
+    }
+}
