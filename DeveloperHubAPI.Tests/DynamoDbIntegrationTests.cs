43eec58963f7dc2f2d64f67722644dd9f7128480
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-03-11 14:46:32 +0000

    Update E2E tests

--- DeveloperHubAPI.Tests/DynamoDbIntegrationTests.cs
+++ DeveloperHubAPI.Tests/DynamoDbIntegrationTests.cs
@@ -34,6 +34,7 @@ namespace DeveloperHubAPI.Tests
 
             EnsureEnvVarConfigured("DynamoDb_LocalMode", "true");
             EnsureEnvVarConfigured("DynamoDb_LocalServiceUrl", "http://localhost:8000");
+            EnsureEnvVarConfigured("googleGroups", "e2e-testing");
             _factory = new DynamoDbMockWebApplicationFactory<TStartup>(_tables);
             Client = _factory.CreateClient();
         }
